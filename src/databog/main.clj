991e3735983c4a91a6830e6cbb6353089226f1b2
Ivan Willig <iwillig@gmail.com>
2024-12-06 09:35:06 -0500

    Add more types

--- src/databog/main.clj
+++ src/databog/main.clj
@@ -1,24 +1,34 @@
 (ns databog.main
-  (:require [typed.clojure :as t]))
+  (:require
+    [typed.clojure :as t]))
 
+
+(t/defalias Attribute t/Keyword)
 (t/defalias Id t/Num)
 (t/defalias TempId t/Num)
 
-(t/defalias TxDatom t/Any)
+(t/defalias DatomValue (t/U ID))
+
+(t/defalias TxDatom  (t/HVec [t/Keyword ID Attribute DatomValue]))
+
 (t/defalias TxDatoms (t/Vec TxData))
 
 
 (defrecord IdMapping
   [temp-id db-id])
 
+
 (t/ann-record IdMapping [temp-id :- TempId db-id :- Id])
 
 (t/defalias IdMappings (t/Vec IdMapping))
 
+
 (defrecord TxDataPart
   [tx-data id-mappings])
 
+
 (t/ann-record TxDataPart [tx-data :- t/Any id-mappings :- IdMappings])
+
 
 (defn id-mapping
   "Given a key attribute and a temp id
@@ -27,7 +37,9 @@
   (map->IdMapping {:temp-id temp-id
                    :db-id   db-id}))
 
+
 (t/ann id-mapping [TempId Id -> IdMapping])
+
 
 (defn tx-data
   ([id-key-mapping tx-data]
@@ -37,11 +49,16 @@
                        id-key-mapping
                        [id-key-mapping])})))
 
+
 (t/ann tx-data [(t/Vec IdMapping)
                 (t/Vec TxDatom) -> TxDataPart])
+
 
 (defn combine-tx-data
   [& tx-data-seqs]
   (tx-data
     (vec (mapcat :id-mappings tx-data-seqs))
     (vec (mapcat :tx-data tx-data-seqs))))
+
+
+(t/ann combine-tx-data [TxDataPart :* :-> TxDataPart])
