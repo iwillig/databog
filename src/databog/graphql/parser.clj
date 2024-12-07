2a6e175d048c71387a7f63273a3423933a6c133d
Ivan Willig <iwillig@gmail.com>
2024-12-07 15:49:17 -0500

    Type GraphQL parser

--- src/databog/graphql/parser.clj
+++ src/databog/graphql/parser.clj
@@ -1,12 +1,25 @@
 (ns databog.graphql.parser
   (:require
+   [typed.clojure :as t]
    [clj-antlr.core :as antlr]
    [clojure.java.io :as io]))
+
+(t/ann clojure.core/slurp [(t/U t/Str
+                                nil
+                                java.net.URL) -> t/Str])
+(t/ann antlr/parser [t/Str -> t/Any])
+
+(t/ann antlr/parse  [t/Str (t/HMap :complete? false) t/Str -> t/Any])
+
+
 
 (set! *warn-on-reflection* true)
 
 (def default-antlr-options
   {:throw? false})
+
+(t/ann default-antlr-options
+       (t/HMap :mandatory {:throw? false} :complete? true))
 
 ;; Taken from Lacinia
 (defn compile-grammar
@@ -16,8 +29,15 @@
    (slurp)
    (antlr/parser)))
 
+(t/ann compile-grammar [t/Str -> t/Any])
+
+(t/defalias Gramar t/Any)
+
 (def graphql-grammar (compile-grammar "Graphql.g4"))
+(t/ann graphql-grammar t/Str)
 (def schema-grammar (compile-grammar "schema.g4"))
+(t/ann schema-grammar t/Str)
+
 
 (def ^:private ignored-terminals
   "Textual fragments which are to be immediately discarded as they have no
@@ -25,9 +45,14 @@
   #{"'{'" "'}'" "'('" "')'" "'['" "']'" "'...'" "'fragment'" "'on'" "type" "&"
     "':'" "'='" "'$'" "'!'" "\"" "'@'"})
 
+(t/ann ignored-terminals
+       (t/Set t/Str))
+
 (defn ignored-terminal?
   [token-name]
   (contains? ignored-terminals token-name))
+
+(t/ann ignored-terminal? [t/Str -> t/Bool])
 
 (defn parse
   "Given a Grammar file and a GraphQL String
@@ -35,9 +60,14 @@
   [grammar gql-string]
   (antlr/parse grammar default-antlr-options gql-string))
 
+(t/ann parse
+       [t/Str t/Str -> t/Any])
+
 (defn parse-schema
   [schema-string]
   (parse schema-grammar schema-string))
+
+
 
 (defn parse-schema-file
   [path]
